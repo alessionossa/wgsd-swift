1729672a6eae0c21ee656759a86ef9ba7998cba8
Alessio Nossa <alessio.nossa@gmail.com>
2021-12-23 10:54:48 +0100

    Added test

--- Tests/wgsd-swiftTests/wgsd_swiftTests.swift
+++ Tests/wgsd-swiftTests/wgsd_swiftTests.swift
@@ -3,9 +3,20 @@ import XCTest
 
 final class wgsd_swiftTests: XCTestCase {
     func testExample() throws {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct
-        // results.
-        XCTAssertEqual(wgsd_swift().text, "Hello, World!")
+        
+        let expectation = self.expectation(description: "Query 1")
+        
+        wgsd_swift.queryServer(dnsServer: "164.90.235.103",
+                               port: 5356,
+                               dnsZone: "exampleniodns.org.",
+                               peersPubKey: ["F9dNYWfLLMYBwNXMgnmoOW8W66WNwUogr+rvWaTW4gU="]
+        ){ endpoints in
+            print("Got the results:")
+            print(endpoints)
+            
+            expectation.fulfill()
+        }
+        
+        waitForExpectations(timeout: 5)
     }
 }
